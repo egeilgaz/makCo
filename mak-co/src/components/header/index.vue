6b6f3e39fc23c5e251a3cd03ac6841f9ab409630
Ege <ilgaz-ege@hotmail.com>
2021-12-15 00:26:08 +0300

    v9

--- mak-co/src/components/header/index.vue
+++ mak-co/src/components/header/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="fixed w-full z-20">
-    <div class="bg-white h-32 grid grid-cols-3">
+    <div class="bg-white h-28 md:h-32 grid grid-cols-3">
       <div class="flex flex-row-reverse py-5 pl-6"></div>
-      <div class="w-24 h-24 mx-auto my-auto">
+      <div class="w-20 h-20 md:w-24 md:h-24 mx-auto my-auto">
         <img src="../../assets/makco-logo.jpeg" alt="" />
       </div>
       <div class="flex flex-row-reverse py-8 px-4">
